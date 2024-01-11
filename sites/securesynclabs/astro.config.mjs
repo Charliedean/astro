a1602f69982834d71c831c880d91e111844ed49a
Charlie Dean <dean.charlie07@gmail.com>
2024-01-11 19:32:26 +0000

    update site www

--- sites/securesynclabs/astro.config.mjs
+++ sites/securesynclabs/astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://securesynclabs.com',
+	site: 'https://www.securesynclabs.com',
 });
